78b0e93ab4b6cf2a028f9737a907ab32a0f72eda
Emily Su <emilyzqsu@gmail.com>
2024-01-20 16:18:54 -0500

    Finished script to download datasets and uploaded raw data

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 ## Overview
 
+# Important Notes
+**About unedited_collisions_data.csv*: This CSV file is currently not in `inputs/data`
+due to its file size exceeding 100MB. To download unedited_collisions_data.csv, 
+run the script `01-download_data.R` located in the following path: `scripts/01-download_data.R`. 
+**Statement on LLM usage: No LLMs were used for any aspect of this work.**
+
 ## File Structure
 
 The repo is structured as:
@@ -10,6 +16,3 @@ The repo is structured as:
 -   `outputs/data` contains the cleaned dataset that was constructed.
 -   `outputs/paper` contains the files used to generate the paper, including the Quarto document and reference bibliography file, as well as the PDF of the paper. 
 -   `scripts` contains the R scripts used to simulate, download and clean data.
-
-# Note
-**Statement on LLM usage: No LLMs were used for any aspect of this work.**
--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -7,24 +7,45 @@
 # License: MIT
 # Pre-requisites: opendatatoronto and tidyverse has been installed 
 # by running 00-install_packages.R
+# IMPORTANT NOTE: The "Police Annual Statistical Report - Traffic Collisions" dataset, 
+# is a large file so it might take a while to finish downloading and saving the file. 
 
 # Datasets that will be downloaded and saved:
-# 1. Police Annual Statistical Report - Traffic Collisions: https://open.toronto.ca/dataset/police-annual-statistical-report-traffic-collisions/
+# 1. Ward Profiles (25-Ward Model): https://open.toronto.ca/dataset/ward-profiles-25-ward-model/
 # 2. City Wards - Traffic Collisions: https://open.toronto.ca/dataset/city-wards/
-# 3. Ward Profiles (25-Ward Model): https://open.toronto.ca/dataset/ward-profiles-25-ward-model/
+# 3. Police Annual Statistical Report - Traffic Collisions: https://open.toronto.ca/dataset/police-annual-statistical-report-traffic-collisions/
 
 #### Workspace setup ####
 # Loading packages
 library(opendatatoronto)
 library(tidyverse)
 
-#### Download data ####
 
+#### Download and Save data ####
+# Code snippet to filter for and download datasets was adapted from: 
+# https://open.toronto.ca/dataset/police-annual-statistical-report-traffic-collisions/
 
+# Function to download files
+download_csv_geojson <- function(files) {
+  filter(files, row_number()==1) |> get_resource()
+}
 
-#### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+# Download "Ward Profiles (25-Ward Model)" dataset
+ward_profiles_data <- download_csv_geojson(list_package_resources("6678e1a6-d25f-4dff-b2b7-aa8f042bc2eb"))
+# Save "Ward Profiles (25-Ward Model)" dataset
+write_csv(as.data.frame(ward_profiles_data[1]), "inputs/data/unedited_ward_profiles_data.csv")
 
-         
+# Download "City Wards" dataset
+# Filter for CSV and geojson files in the package resources list
+files_one <- filter(list_package_resources("5e7a8234-f805-43ac-820f-03d7c360b588"), tolower(format) %in% c('csv', 'geojson'))
+city_wards_data <- download_csv_geojson(files_one)
+# Save "City Wards" dataset as a R object 
+# Code snippet adapted from https://fcostartistician.wordpress.com/2017/09/13/how-to-deal-with-spatial-data-in-r-and-ggplot-shapefiles/ 
+saveRDS(city_wards_data, 'inputs/data/city_wards.RDS')
+
+# Download "Police Annual Statistical Report - Traffic Collisions" dataset
+# Filter for CSV and geojson files in the package resources list
+files_two <- filter(list_package_resources("ec53f7b2-769b-4914-91fe-a37ee27a90b3"), tolower(format) %in% c('csv'))
+collisions_data <- download_csv_geojson(files_two)
+# Save "Police Annual Statistical Report - Traffic Collisions" dataset
+write_csv(collisions_data, "inputs/data/unedited_collisions_data.csv")
